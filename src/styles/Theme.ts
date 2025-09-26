a70d2b1750b017b0b0c39f95dffa45c1cfa049dd
Israel-ops-cmd <israellipe2020@gmail.com>
2025-09-26 10:57:07 -0300

    feat: creating global style

---
+++ src/styles/Theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+    colors: {
+        primary: '#fefefe',
+        text: 'black',
+        card_1: '#fbda63',
+        card_2: '#f27271',
+        card_3: '#5fc192',
+        card_4: '#fe9641',
+        button: '#2c86d7'
+    }
+}
